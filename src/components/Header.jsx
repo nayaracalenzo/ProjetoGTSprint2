b20ff214308f062c661fd6865a11fa8a35bd22c6
nayaracalenzo <queiroz.14@hotmail.com>
2025-06-12 15:00:35 -0300

    teste cypress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import { FaCartShopping } from 'react-icons/fa6'
-import { FaBars } from 'react-icons/fa'
+import React from "react";
+import { FaCartShopping } from "react-icons/fa6";
+import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="bg-[#d0d0d2] p-10 flex justify-between items-center">
-      <h1 className='font-bold text-5xl text-pink-600'>Header</h1>
-    <nav>
-        <ul className='flex gap-2'>
-            <li>
-              <FaCartShopping/>
-            </li>
-            <li>
-              <FaBars/>
-            </li>
+      <h1 className="font-bold text-5xl text-pink-600">Header</h1>
+      <nav>
+        <ul className="flex gap-2">
+          <li>
+            <Link to={"/carrinho"}>
+              <FaCartShopping />
+            </Link>
+          </li>
+          <li>
+            <FaBars />
+          </li>
         </ul>
-    </nav>
-</header>
-  )
-}
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
